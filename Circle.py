f5ecc6db6665f5c43b9082e5cbb73f54c18dd105
KELECHIDAVIS <kboss16058@gmail.com>
2025-06-15 19:13:59 -0700

    first commit

---
+++ Circle.py
@@ -0,0 +1,5 @@
+class Circle:
+    def __init__(self, pos , radius, color ):
+        self.pos = pos
+        self.radius = radius
+        self.color = color #(r,g,b,a)
---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+Evolve Agents to recreate image over generations
+their genome is going to be the shapes they have
+the fitness is going to be the mean squared error from the target image (lower is better)
+
+starting out the agents have a random circle in a random place
+we can detect the color of each of the squares by adding the values of all the circles with the
+color of the background without using overhead of the pygame
+'''
+
